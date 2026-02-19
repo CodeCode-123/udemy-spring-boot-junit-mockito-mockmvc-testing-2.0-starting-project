8fd13ca6406e4285f3dc97b7663c2ab73d04f3cf
Shen Tang <stang311@gatech.edu>
2026-02-19 18:17:43 -0500

    Added Spring Boot Unit Testing Support with reading application.properties, injecting beans, and assertion tests

--- src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -3,13 +3,17 @@ import com.luv2code.component.MvcTestingExampleApplication;
 import com.luv2code.component.models.CollegeStudent;
 import com.luv2code.component.models.StudentGrades;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 public class ApplicationExampleTest {
@@ -27,6 +31,8 @@ public class ApplicationExampleTest {
     CollegeStudent student;
     @Autowired
     StudentGrades studentGrades;
+    @Autowired
+    ApplicationContext context;
 
     @BeforeEach
     public void beforeEach() {
@@ -39,9 +45,65 @@ public class ApplicationExampleTest {
         studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList(100.0, 85.0, 76.50, 91.75)));
         student.setStudentGrades(studentGrades);
     }
-
+    @DisplayName("Add grade results for student grades")
     @Test
-    void basicTest() {
-
+    public void addGradeResultsForStudentGrades() {
+        assertEquals(353.25, studentGrades.addGradeResultsForSingleClass(
+                student.getStudentGrades().getMathGradeResults()
+        ));
+    }
+    @DisplayName("Add grades results for student grades not equal")
+    @Test
+    public void addGradeResultsForStudentGradesNotEqual() {
+        assertNotEquals(0, studentGrades.addGradeResultsForSingleClass(
+                student.getStudentGrades().getMathGradeResults()
+        ));
+    }
+    @DisplayName("Is grade greater")
+    @Test
+    public void isGradeGreaterStudentGrades() {
+        assertTrue(studentGrades.isGradeGreater(90,75),
+                "failure - should be true");
+    }
+    @DisplayName("Is grade greater false")
+    @Test
+    public void isGradeGreaterFalseStudentGrades() {
+        assertFalse(studentGrades.isGradeGreater(89, 92),
+                "failure - should be false");
+    }
+    @DisplayName("Check Null for student grades")
+    @Test
+    public void checkNullForStudentGrades() {
+        assertNotNull(studentGrades.checkNull(student.getStudentGrades().getMathGradeResults()),
+                "object should not be null");
+    }
+    @DisplayName("Create student without grade init")
+    @Test
+    public void createStudentWithoutGradesInit() {
+        // in the MvcTestingExampleApplication, a bean named collegeStudent is defined
+        CollegeStudent studentTwo = context.getBean("collegeStudent", CollegeStudent.class);
+        studentTwo.setFirstname("Chad");
+        studentTwo.setLastname("Darby");
+        studentTwo.setEmailAddress("chad.darby@luv2code_school.com");
+        assertNotNull(studentTwo.getFirstname());
+        assertNotNull(studentTwo.getLastname());
+        assertNotNull(studentTwo.getEmailAddress());
+        assertNull(studentGrades.checkNull(studentTwo.getStudentGrades()));
+    }
+    @DisplayName("Verify students are prototype")
+    @Test
+    public void verifyStudentsArePrototye() {
+        CollegeStudent studentTwo = context.getBean("collegeStudent", CollegeStudent.class);
+        assertNotSame(student, studentTwo);
+    }
+    @DisplayName("Find Grade Point Average")
+    @Test
+    public void findGradePointAverage() {
+        assertAll("Testing all assertEquals",
+                ()-> assertEquals(353.25, studentGrades.addGradeResultsForSingleClass(
+                        student.getStudentGrades().getMathGradeResults())),
+                ()-> assertEquals(88.31, studentGrades.findGradePointAverage(
+                        student.getStudentGrades().getMathGradeResults()))
+        );
     }
 }
